2ece8bed2a78e3e740e8229f2b9616c916bb3dbc
itrack <itrack@debian-BULLSEYE-live-builder-AMD64>
2021-09-07 14:40:27 -0300

    feat(first files): struct folder the node

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'mysql',
+  host: 'localhost',
+  username: 'database-mysql',
+  password: '1298',
+  database: 'gobarber',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredALL: true,
+  },
+};
