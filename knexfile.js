3dc2ea1dc86739ce5109eb063705a9e278268c1b
itrack <itrack@debian-BULLSEYE-live-builder-AMD64>
2021-09-15 12:13:44 -0300

    feat(knexfile): added config conection in database

---
+++ knexfile.js
@@ -0,0 +1,53 @@
+// Update with your config settings.
+
+module.exports = {
+  development: {
+    client: 'mysql2',
+    connection: {
+      host: '127.0.0.1',
+      port: 3308,
+      user: 'teste',
+      password: '1298',
+      database: 'reload',
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: `${__dirname}/src/database/migrations`
+    },
+    seeds: {
+      directory: `${__dirname}/src/database/seeds`
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password',
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+    },
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password',
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+    },
+  },
+};
