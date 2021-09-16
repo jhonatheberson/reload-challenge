a8757ce488976630dcf9c871d95b70fb3a7a81a3
itrack <itrack@debian-BULLSEYE-live-builder-AMD64>
2021-09-16 03:10:58 -0300

    feat(migrations company): added relationship with user 1-n

--- src/database/migrations/20210915200120_create_tables_company.js
+++ src/database/migrations/20210915200120_create_tables_company.js
@@ -1,3 +1,9 @@
+/**
+ * this function creates the migrations to the company table related database
+ * @summary This function uses knex to create the table as its columns in a simple and efficient way.
+ * @param {Knex.JS} knex - knex is a library that makes it possible to perform migrations in a simple way
+ * @return {Knex.JS} it returns the knex schema to create the table
+ */
 exports.up = (knex) =>
   knex.schema.createTable('company', (table) => {
     table.increments('id');
@@ -13,8 +19,18 @@ exports.up = (knex) =>
     table.string('latitude', [6]);
     table.string('longitude', [6]);
 
+    table.integer('user_id').unsigned().notNullable();
+    // relationship 1 - n
+    table.foreign('user_id').references('id').inTable('users');
+
     table.timestamp('create_at').defaultTo(knex.fn.now());
     table.timestamp('update_at').defaultTo(knex.fn.now());
   });
 
+/**
+ * this function delete the migrations to the company table related database
+ * @summary This function uses knex to delete the table as its columns in a simple and efficient way.
+ * @param {Knex.JS} knex - knex is a library that makes it possible to perform migrations in a simple way
+ * @return {Knex.JS} it returns the knex schema to delete the table
+ */
 exports.down = (knex) => knex.schema.dropTable('company');
