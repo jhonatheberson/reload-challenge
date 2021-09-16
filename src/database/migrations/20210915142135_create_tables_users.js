26edc0cb1fdc31574732d7e3877b07c4e57bd4bf
itrack <itrack@debian-BULLSEYE-live-builder-AMD64>
2021-09-16 03:12:50 -0300

    docs(migrations_users): added docstring and finished migrations

--- src/database/migrations/20210915142135_create_tables_users.js
+++ src/database/migrations/20210915142135_create_tables_users.js
@@ -1,3 +1,10 @@
+/**
+ * this function creates the migrations to the users table related database
+ * @summary This function uses knex to create the table as its columns in a simple and efficient way.
+ * @param {Knex.JS} knex - knex is a library that makes it possible to perform migrations in a simple way
+ * @return {Knex.JS} it returns the knex schema to create the table
+ */
+
 exports.up = (knex) =>
   knex.schema.createTable('users', (table) => {
     table.increments('id');
@@ -9,4 +16,10 @@ exports.up = (knex) =>
     table.timestamp('update_at').defaultTo(knex.fn.now());
   });
 
+/**
+ * this function delete the migrations to the users table related database
+ * @summary This function uses knex to delete the table as its columns in a simple and efficient way.
+ * @param {Knex.JS} knex - knex is a library that makes it possible to perform migrations in a simple way
+ * @return {Knex.JS} it returns the knex schema to delete the table
+ */
 exports.down = (knex) => knex.schema.dropTable('users');
