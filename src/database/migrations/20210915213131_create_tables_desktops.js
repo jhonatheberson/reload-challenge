0c0c38c8e32e0b59e7aff24bb0275d2b388c9cb4
itrack <itrack@debian-BULLSEYE-live-builder-AMD64>
2021-09-16 03:05:33 -0300

    docs(migrations desktops): added docstring

--- src/database/migrations/20210915213131_create_tables_desktops.js
+++ src/database/migrations/20210915213131_create_tables_desktops.js
@@ -1,3 +1,12 @@
+/**
+ * this function creates the migrations to the desktops table related database
+ * @summary This function uses knex to create the table as its columns in a simple and efficient way.
+ * @param {Knex.JS} knex - knex is a library that makes it possible to perform migrations in a simple way
+ * @return {Knex.JS} it returns the knex schema to create the table
+ */
+
+const { Knex } = require('knex');
+
 exports.up = (knex) =>
   knex.schema.createTable('desktops', (table) => {
     table.increments('id');
@@ -11,5 +20,12 @@ exports.up = (knex) =>
 
     table.timestamps(true, true);
   });
+
+/**
+ * this function delete the migrations to the desktops table related database
+ * @summary This function uses knex to delete the table as its columns in a simple and efficient way.
+ * @param {Knex.JS} knex - knex is a library that makes it possible to perform migrations in a simple way
+ * @return {Knex.JS} it returns the knex schema to delete the table
+ */
 
 exports.down = (knex) => knex.schema.dropTable('desktops');
