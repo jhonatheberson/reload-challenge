8b771ad845bc30937d390d504c1db2b04d6c367d
itrack <itrack@debian-BULLSEYE-live-builder-AMD64>
2021-09-16 02:37:51 -0300

    docs(docstring): added docstring and commits in code

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,19 @@
 import { Router } from 'express';
-// import User from './app/models/User';
 
+// controller
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import CompanyController from './app/controllers/CompanyController';
 import DesktopsController from './app/controllers/DesktopsController';
 import ContributorsController from './app/controllers/ContributorsController';
 
+// middlewares
 import authMiddleware from './app/middleware/auth';
 import errorMiddleware from './app/middleware/error';
 
 const routes = new Router();
+
+routes.use(errorMiddleware); // middleware error global
 
 // routes users
 routes.post('/users', UserController.store);
@@ -19,14 +22,13 @@ routes.get('/users', UserController.index);
 // routes sessions
 routes.post('/sessions', SessionController.store);
 
+// this middleware only runs after it is declared
+// soon routes posts above does not run this middleware
+routes.use(authMiddleware); // middleware auth global
+
 // routes company
 routes.get('/company', CompanyController.index);
 routes.post('/company', CompanyController.store);
-
-// esse middleware so é executado apos ele ser declarado.
-// logo as rotas posts acima não é executado esse middleware
-routes.use(errorMiddleware); // middleware global de auth
-routes.use(authMiddleware); // middleware global de auth
 
 //routes desktops
 routes.get('/desktops', DesktopsController.index);
