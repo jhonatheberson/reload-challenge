cff545f29960e4c7e5d8941b980b551ff7d1f593
itrack <itrack@debian-BULLSEYE-live-builder-AMD64>
2021-09-16 02:48:15 -0300

    docs(docstring): added docstring and commits in code

--- src/app.js
+++ src/app.js
@@ -1,26 +1,28 @@
-import express from 'express'; // sucrase faz isso
+import express from 'express'; // sucrase allows this kind of import
 import routes from './routes';
 
-import './database';
-
+/**
+ * Class App that starts Node builders, with express
+ *
+ */
 class App {
   constructor() {
-    // esse metodo é contrutor é chamado
-    // automaticamente ao chamar a classe App
+    // this method is constructor is called
+    // automatically when calling the App class
     this.server = express();
 
     this.middlewares();
     this.routes();
   }
-
+  // constructor middlewares
   middlewares() {
     this.server.use(express.json());
   }
-
+  // constructor routes
   routes() {
     this.server.use(routes);
   }
 }
 
-// module.exports = new App().server; //esportanto o class App, o server
-export default new App().server; // sucrase faz isso
+// module.exports = new App().server; without sucrase it looks like this!
+export default new App().server;
